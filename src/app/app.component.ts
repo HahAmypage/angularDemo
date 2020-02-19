fe385e81f5d35d87a7203edd6efdd005d4d3136b
HahAmypage <13632439133@139.com>
2020-02-19 15:04:15 +0800

    封装一个组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 // 使用interface定义属性
 //属性?:类型  代表可选属性
 //readonly 属性:类型  代表只读属性，赋值一次后不能重复赋值
-interface TopMenu{
-  title:string,
-  link:string
-}
+
 
 // interface定义方法
 // interface addFunc{
@@ -25,94 +22,7 @@ interface TopMenu{
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '拼多多';
-  selectIndex = -1 ; // 起始值
-  hanlderSelection(index:number) {
-    this.selectIndex = index;
-  }
-  menus:TopMenu[] = [
-    {
-      title:'热门',
-      link:''
-    },
-    {
-      title:'女装',
-      link:''
-    },
-    {
-      title:'鞋包',
-      link:''
-    },
-    {
-      title:'男装',
-      link:''
-    },
-    {
-      title:'食品',
-      link:''
-    },
-    {
-      title:'母婴',
-      link:''
-    },
-    {
-      title:'手机',
-      link:''
-    },
-    {
-      title:'百货',
-      link:''
-    },
-    {
-      title:'内衣',
-      link:''
-    },
-    {
-      title:'电器',
-      link:''
-    },
-    {
-      title:'家纺',
-      link:''
-    },
-    {
-      title:'水果',
-      link:''
-    },
-    {
-      title:'家具',
-      link:''
-    },
-    {
-      title:'美妆',
-      link:''
-    },
-    {
-      title:'家装',
-      link:''
-    },
-    {
-      title:'运动',
-      link:''
-    },
-    {
-      title:'汽车',
-      link:''
-    },
-    {
-      title:'医药',
-      link:''
-    },
-    {
-      title:'电脑',
-      link:''
-    },
-    {
-      title:'海淘',
-      link:''
-    }
-  ];
-
+  
   // add:addFunc = (x,y) => x + y;
 
   // dict:Dict = {
