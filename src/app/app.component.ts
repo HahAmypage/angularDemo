4f019e85d86bd572f618a96b798c1adc5e9a3b9f
HahAmypage <13632439133@139.com>
2020-02-19 12:33:07 +0800

    顶部菜单

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+// 使用interface定义属性
+//属性?:类型  代表可选属性
+//readonly 属性:类型  代表只读属性，赋值一次后不能重复赋值
+interface TopMenu{
+  title:string,
+  link:string
+}
+
+// interface定义方法
+// interface addFunc{
+//   // 表示方法有两个入参，都是number类型，冒号后面是返回值类型（number）
+//   (x:number,y:number):number;
+// }
+
+
+// interface定义索引类型
+// interface Dict{
+//   [key: string]:string;
+// }
 
 @Component({
   selector: 'app-root',
@@ -7,5 +26,102 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '拼多多';
-  tabs = ['热门','男装','女装','电脑','手机'];
+  selectIndex = -1 ; // 起始值
+  hanlderSelection(index:number) {
+    this.selectIndex = index;
+  }
+  menus:TopMenu[] = [
+    {
+      title:'热门',
+      link:''
+    },
+    {
+      title:'女装',
+      link:''
+    },
+    {
+      title:'鞋包',
+      link:''
+    },
+    {
+      title:'男装',
+      link:''
+    },
+    {
+      title:'食品',
+      link:''
+    },
+    {
+      title:'母婴',
+      link:''
+    },
+    {
+      title:'手机',
+      link:''
+    },
+    {
+      title:'百货',
+      link:''
+    },
+    {
+      title:'内衣',
+      link:''
+    },
+    {
+      title:'电器',
+      link:''
+    },
+    {
+      title:'家纺',
+      link:''
+    },
+    {
+      title:'水果',
+      link:''
+    },
+    {
+      title:'家具',
+      link:''
+    },
+    {
+      title:'美妆',
+      link:''
+    },
+    {
+      title:'家装',
+      link:''
+    },
+    {
+      title:'运动',
+      link:''
+    },
+    {
+      title:'汽车',
+      link:''
+    },
+    {
+      title:'医药',
+      link:''
+    },
+    {
+      title:'电脑',
+      link:''
+    },
+    {
+      title:'海淘',
+      link:''
+    }
+  ];
+
+  // add:addFunc = (x,y) => x + y;
+
+  // dict:Dict = {
+  //   a:'111',
+  //   b:'222'
+  // }
+
+  // constructor(){
+  //   console.log(this.dict.b);
+  // }
 }
+
